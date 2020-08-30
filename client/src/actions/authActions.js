792eb926a13dce7c351b46de3e84a9490051ec9a
alaamagdyomar <alaa_magdy@outlook.com>
2020-08-30 23:55:17 +0200

    Redux login action & set current user

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,6 +1,9 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
+// Register user
 export const registerUser = (userData, history) => (dispatch) => {
   axios
     .post("/api/users/register", userData)
@@ -11,4 +14,36 @@ export const registerUser = (userData, history) => (dispatch) => {
         payload: err.response.data,
       })
     );
+};
+
+// Login User
+export const loginUser = (userData) => (dispatch) => {
+  axios
+    .post("/api/users/login", userData)
+    .then((res) => {
+      // save to localStorage
+      const { token } = res.data;
+      // set token to localStorage
+      localStorage.setItem("jwtToken", token);
+      // set token to auth header
+      setAuthToken(token);
+      //Decode token to get user data
+      const decoded = jwt_decode(token);
+      // set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
+
+// set logged in user
+export const setCurrentUser = (decoded) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded,
+  };
 };
